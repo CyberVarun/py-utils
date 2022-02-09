8d9c9c528dce1eefbc88514c80c18d106d959d25
CyberVarun <varunjagtap424@gmail.com>
2022-02-09 13:15:08 +0530

    Added scapy network sniffer in scapy_scan.py

--- scapy_scan.py
+++ scapy_scan.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 
-import typer
 from scapy.all import *
+from typing import Optional
+import typer
+import scan
 
 VERSION="v0.1"
+CREDITS = '		   Created by @CyberVarun (https://github.com/CyberVarun) '
+
 app = typer.Typer()
 
 @app.command()
 def nscn(
 	net: str = typer.Option(..., '--net', '-n', help="Specify network address."),
+	rang: Optional[str] = typer.Option(None, '--range', '-r', help="Specify network range (8/16/24). default is (IP/24)."),
 	output: bool = False,
 ):
 	"""
-	Network Scanner using scapy
+	To scan network using scapy module.
 	"""
 	LOGO='''
 			 _   _      _          _____                 
@@ -22,15 +27,27 @@ def nscn(
 			| |\  |  __/ |_       ____) | (_| (_| | | | |
 			|_| \_|\___|\__|     |_____/ \___\__,_|_| |_|'''
 
-	network = net + "/24"
+	# Specifying the network range IP/rang. Defautl is IP/24 (127.0.0.1/24). 
+	if rang is None: 
+		network = net + "/24"
+	else:
+		network = net + "/" + rang
+
+	# Empty list for data output (--output option)
 	data_list2 = {}
+
+	# Printing Logo, creadits and version
 	typer.secho(LOGO, fg=typer.colors.BRIGHT_CYAN)
 	typer.secho(f"{CREDITS}{VERSION}\n", fg=typer.colors.BRIGHT_CYAN)
 	
 	try:
+		# Broadcasting ARP (Address Resolution Protocol) requestest into network.
 		addresses = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=network), timeout=1, verbose=False)[0]
+		
 		for addr in addresses:
-			typer.secho(f"Host: {addr[1].psrc}		Mac: {addr[1].hwsrc}", fg=typer.colors.BRIGHT_GREEN)
+			# Grabbing devices IP(.psrc) and MAC(.hwsrc) that replied to the request. 
+			typer.secho(f"IP: {addr[1].psrc}		MAC: {addr[1].hwsrc}", fg=typer.colors.BRIGHT_GREEN)
+			# Adding data to data_list2 for output.
 			data_list2[addr[1].psrc] = "live"
 	
 	except Exception as error:
@@ -39,9 +56,48 @@ def nscn(
 	
 	except KeyboardInterrupt:
 		typer.secho("[!] Keyboard Interrupt", fg=typer.colors.RED)
+		exit()
 
+	# Creating output data in .json format if output option is set to true
 	scn = 0
 	scan.data_output(net, output, data_list2, scn)
+
+@app.command()
+def sniffer(
+	interface: str = typer.Option(..., '--iface', '-i', help="Specify network interface(wlan0/eth0)."),
+	count: Optional[int] = typer.Option(None, '--count', '-c', help="Specify timeout. By default is will do live sniffing."),
+	pkt_type: str = typer.Option(..., '--type', '-t', help="Specify packet typer(tcp/ip)."),
+):	
+
+	"""
+	To sniff network traffic.
+	"""
+	try:
+		if count is None and pkt_type == "all":
+			# sniffing all types of packets.
+			sniff(iface=interface, prn = lambda x: x.summary())
+		
+		elif count != None and  pkt_type == "all":
+			# sniffing selected but all types of packets.
+			s = sniff(iface=interface, count=count)
+			s.summary()
+
+		elif count is None:
+			# sniffing all but filtered packets.
+			sniff(iface=interface, filter=pkt_type, prn = lambda x: x.summary())
+		
+		else:
+			# Sniffing selected and filtered packets. 	
+			s = sniff(iface=interface, count=count, filter=pkt_type)
+			s.summary()
+
+	except Exception as error:
+		# print(error)
+		pass
+
+	except KeyboardInterrupt:
+		typer.secho("[!] Keyboard Interrupt", fg=typer.colors.RED)
+		exit()
 
 if __name__ == "__main__":
 	app()
