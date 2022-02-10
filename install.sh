73e42112b8f1abf44fb20e2f235359c5a9c71462
CyberVarun <varunjagtap424@gmail.com>
2022-02-10 16:02:40 +0530

    Modified as a one line installer

--- install.sh
+++ install.sh
@@ -1,46 +1,63 @@
 #!/bin/bash
 
-grn='\033[32m'
-ylo='\033[1;33m'
-red='\033[31m'
+GRN='\033[32m'
+YLO='\033[1;33m'
+RED='\033[31m'
 
-if [[ $UID != 0 ]];
-then 
-	echo -e "${ylo}[!] Run with sudo privileges."
-	exit
-fi
+TOR_START_SUCC="[*] Started Tor service. 
+[*] You can use proxychains now.
+[*] Command: proxychains ./crack.py [Options]"
 
-sudo apt install proxychains tor -y > /dev/null 2>&1
+TOR_START_FAIL="[!] Failed to start tor service.
+[!] Check if any service is already using port 9050 & run the script again."
+
+cd ~
+git clone https://github.com/CyberVarun/py-utils.git 
+
+sudo apt install proxychains tor python3 python3-pip -y 2> /dev/null
 
 if [[ $? == 0 ]];
 then 
-	echo -e "${grn}[*] Installed proxychains and tor Succesfully."
+	echo -e "${GRN}[*] Installed proxychains and tor Succesfully."
 else
-	echo -e "${red}[!] Installation Failed."
-	echo -e "${ylo}[!] Check internet connection & run the script again."
+	echo -e "${RED}[!] Installation Failed."
+	echo -e "${YLO}[!] Check internet connection & run the script again."
 	exit
 fi
 
+cd py-utils/
 pip install -r requirements.txt > /dev/null 2>&1
 
 if [[ $? == 0 ]];
 then
-	echo -e "${grn}[*] Installed requirements Succesfully."
+	echo -e "${GRN}[*] Installed requirements Succesfully."
 else
-	echo -e "${ylo}[!] Failed to install requirements."
-	echo -e "${ylo}[!] Check internet connection & run the script again."
+	echo -e "${YLO}[!] Failed to install requirements."
+	echo -e "${YLO}[!] Check internet connection & run the script again."
 	exit
 fi
 
-sudo systemctl start tor > /dev/null 2>&1
+systemctl > /dev/null 2>&1
 
 if [[ $? == 0 ]];
-then 
-	echo -e "${grn}[*] Started Tor service."
-	echo -e "${grn}[*] You can use proxychains now."
-	echo -e "${grn}[*] Command: proxychains ./crack.py [Options]"
+then  
+	sudo systemctl start tor > /dev/null 2>&1
+
+	if [[ $? == 0 ]];
+	then 
+		echo -e "${GRN}${TOR_START_SUCC}"
+	else
+		echo -e "${RED}${TOR_START_FAIL}"
+		exit
+	fi
 else
-	echo -e "${red}[!] Failed to start tor service."
-	echo -e "${ylo}[!] Check if any service is already using port 9050 & run the script again."
-	exit
+	sudo service tor start > /dev/null 2>&1
+
+	if [[ $? == 0 ]];
+	then 
+		echo -e "${GRN}${TOR_START_SUCC}"
+	else
+		echo -e "${RED}${TOR_START_FAIL}"
+		exit
+	fi
 fi
